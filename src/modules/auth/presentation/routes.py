6b843b4f011d23d50fcceb16b4544d2d571ccc25
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2026-06-23 22:20:57 -0600

    fix: tipado de casos de uso

--- src/modules/auth/presentation/routes.py
+++ src/modules/auth/presentation/routes.py
@@ -21,13 +21,22 @@ from src.modules.auth.presentation.schemas import (
     ConsentRequestDTO
 )
 
+from src.modules.auth.application.register_user import RegisterUser
+from src.modules.auth.application.login_user import LoginUser
+from src.modules.auth.application.generate_code import GenerateRecoveryCode
+from src.modules.auth.application.send_recovery_code import SendRecoveryCode
+from src.modules.auth.application.verify_code import VerifyRecoveryCode
+from src.modules.auth.application.verify_user import VerifyUser
+from src.modules.auth.application.reset_password import ResetPassword
+from src.modules.auth.application.confirm_consent import ConfirmConsent
+
 router = APIRouter()
 
 
 @router.post("/register", response_model=LoginResponseDTO, status_code=status.HTTP_201_CREATED)
 async def register(
         data: UserRegister,
-        usecase=Depends(register_user_service),
+        usecase: RegisterUser = Depends(register_user_service),
 ):
     return await usecase.execute(
         data
@@ -37,7 +46,7 @@ async def register(
 @router.post("/login", response_model=LoginResponseDTO, status_code=status.HTTP_200_OK)
 async def login(
         data: LoginRequestDTO,
-        usecase=Depends(login_user_service),
+        usecase: LoginUser = Depends(login_user_service),
 ):
     try:
         return await usecase.execute(data.email, data.password)
@@ -57,8 +66,8 @@ async def me(
 @router.post("/recovery/request", status_code=status.HTTP_200_OK)
 async def request_recovery(
         email: EmailStr,
-        generate_uc=Depends(generate_recovery_code_service),
-        send_uc=Depends(send_recovery_code_service),
+        generate_uc: GenerateRecoveryCode = Depends(generate_recovery_code_service),
+        send_uc: SendRecoveryCode = Depends(send_recovery_code_service),
 ):
     code = await generate_uc.execute(email)
     return await send_uc.execute(email, code)
@@ -68,8 +77,8 @@ async def request_recovery(
 async def verify_code(
         usuario_id: str,
         code: str,
-        usecase_code=Depends(verify_recovery_code_service),
-        usecase_auth=Depends(verify_user_service)
+        usecase_code: VerifyRecoveryCode = Depends(verify_recovery_code_service),
+        usecase_auth: VerifyUser = Depends(verify_user_service)
 ):
     if await usecase_code.execute(usuario_id, code):
         return await usecase_auth.execute(usuario_id)
@@ -80,7 +89,7 @@ async def verify_code(
 async def reset_password(
         usuario_id: str,
         new_password: str,
-        usecase=Depends(reset_password_service),
+        usecase: ResetPassword = Depends(reset_password_service),
 ):
     return await usecase.execute(usuario_id, new_password)
 
@@ -89,7 +98,7 @@ async def reset_password(
 async def submit_consentimiento(
     data: ConsentRequestDTO,
     user: AuthenticatedUser = Depends(get_current_user),
-    usecase = Depends(confirm_consent_service)
+    usecase: ConfirmConsent = Depends(confirm_consent_service)
 ):
     try:
         usecase.execute(
