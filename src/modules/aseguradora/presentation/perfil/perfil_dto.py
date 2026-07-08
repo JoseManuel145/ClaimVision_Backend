58c3f413e3775ff89ec7176622ab8e07f4e8bfbd
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2026-07-08 11:55:03 -0600

    fix: dto mapper

--- src/modules/aseguradora/presentation/perfil/perfil_dto.py
+++ src/modules/aseguradora/presentation/perfil/perfil_dto.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
 class PerfilAseguradoraDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: str
     nombre: str
     rfc: str
